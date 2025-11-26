2d200eb506a91ce2401322b853bdf6c1160cd729
MinMin0504 <krisharubia05@gmail.com>
2025-11-26 17:56:29 +0800

    Gallary page

---
+++ data.js
@@ -0,0 +1,38 @@
+const users = [
+    {
+      title: "Cafe",
+      description: "Taking selfie with my sisters in the new cafe near tanauan",
+      location: "Tanauan",
+      picture: "cafe.png"
+    },
+      {
+      title: "Concert in Moa",
+      description: "This is the day of concert of Eunwoo that my mother attended.",
+      location: "Manila, Moa",
+      picture: "concert2.png"
+    },
+       {
+      title: "Concert in Araneta",
+      description: "Here's the concert of japanese band called 'One ok Rock'.This is where we took a selfie before attending the concert",
+      location: "Quezon city, Manila",
+      picture: "selfie.png"
+    },
+       {
+      title: "Concert in Araneta",
+      description: "A picture concert that we took many pictures before attending the concert.",
+      location: "Quezon city, Manila",
+      picture: "concert1.png"
+    },
+      {
+      title: "Wedding of my older sister",
+      description: "This is the wedding day of my dear older sister.",
+      location: "San Pedro Sto tomas, Batangas",
+      picture: "wedding.png"
+    },
+      {
+      title: "Cinema Movie",
+      description: "This is where we watch in the movie called 'Moon' in directors clubs",
+      location: "Pasay City, Manila",
+      picture: "movie.png"
+    }
+  ];
